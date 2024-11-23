47587e61b4f82de2242003e6306cfd54bfb8c33f
maiaimei <1211674185@qq.com>
2024-11-23 22:50:57 +0800

    Learning SpEL

--- third-party-lib-tests/doc/spel/README.md
+++ third-party-lib-tests/doc/spel/README.md
@@ -8,10 +8,28 @@ The Spring Expression Language (SpEL) is a powerful expression language that sup
 
 There are several operators available in the language:
 
-| Type        | Operators                                    |
-| :---------- | :------------------------------------------- |
-| Arithmetic  | +, -, *, /, %, ^, div, mod                   |
-| Relational  | <, >, ==, !=, <=, >=, lt, gt, eq, ne, le, ge |
-| Logical     | and, or, not, &&, \|\|, !                    |
-| Conditional | ?:                                           |
-| Regex       | matches                                      |
+| Type         | Operators                                    |
+| :----------- | :------------------------------------------- |
+| Mathematical | +, -, *, /, %, ^, div, mod                   |
+| Relational   | <, >, ==, !=, <=, >=, lt, gt, eq, ne, le, ge |
+| Logical      | and, or, not, &&, \|\|, !                    |
+| Conditional  | ?:                                           |
+| Regex        | matches                                      |
+
+## The #this and #root variables
+
+The variable #this is always defined and refers to the current evaluation object (against which unqualified references are resolved). The variable #root is always defined and refers to the root context object. Although #this may vary as components of an expression are evaluated, #root always refers to the root.
+
+## Collection Selection
+
+Selection is a powerful expression language feature that allows you to transform some source collection into another by selecting from its entries.
+
+Selection uses the syntax `?[selectionExpression]`. This will filter the collection and return a new collection containing a subset of the original elements. 
+
+Selection is possible upon both lists and maps.
+
+In addition to returning all the selected elements, it is possible to retrieve just the first or the last value. To obtain the first entry matching the selection the syntax is `^[...]` whilst to obtain the last matching selection the syntax is `$[...]`.
+
+## Reference
+
+[https://docs.spring.io/spring-framework/docs/3.2.x/spring-framework-reference/html/expressions.html](https://docs.spring.io/spring-framework/docs/3.2.x/spring-framework-reference/html/expressions.html)
--- third-party-lib-tests/src/test/java/org/example/spel/SpELTest.java
+++ third-party-lib-tests/src/test/java/org/example/spel/SpELTest.java
@@ -1,11 +1,24 @@
 package org.example.spel;
 
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.TypeRef;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import org.example.BaseTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-public class SpELTest {
+public class SpELTest extends BaseTest {
 
   @Test
   public void testString() {
@@ -22,7 +35,7 @@ public class SpELTest {
   }
 
   @Test
-  public void testArithmeticOperators() {
+  public void testMathematicalOperators() {
     ExpressionParser parser = new SpelExpressionParser();
     Assertions.assertEquals(20, parser.parseExpression("19 + 1").getValue(Integer.class));
     Assertions.assertEquals(19, parser.parseExpression("20 - 1").getValue(Integer.class));
@@ -77,6 +90,52 @@ public class SpELTest {
         parser.parseExpression("'valid alphabetic string' matches '[a-zA-Z\\s]+'").getValue(Boolean.class));
     Assertions.assertEquals(Boolean.FALSE,
         parser.parseExpression("'invalid alphabetic string #$1' matches '[a-zA-Z\\s]+'").getValue(Boolean.class));
+  }
+  
+  @Test
+  public void testStandardEvaluationContext() {
+    final HashMap<String, String> map = new HashMap<>();
+    map.put("k1", "v1");
+    map.put("k2", "v2");
+    EvaluationContext context = new StandardEvaluationContext(map);
+    context.setVariable("map", map);
+    List<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    context.setVariable("list", list);
+    ExpressionParser parser = new SpelExpressionParser();
+    Assertions.assertEquals(map, parser.parseExpression("#root").getValue(context, Map.class));
+    Assertions.assertEquals(map, parser.parseExpression("#map").getValue(context, Map.class));
+    Assertions.assertEquals(list, parser.parseExpression("#list").getValue(context, List.class));
+  }
+
+  @Test
+  public void testCollectionSelection_Map() {
+    final HashMap<String, String> map = new HashMap<>();
+    map.put("k1", "v1");
+    map.put("k2", "v2");
+    EvaluationContext context = new StandardEvaluationContext(map);
+    ExpressionParser parser = new SpelExpressionParser();
+    Assertions.assertEquals(2, Objects.requireNonNull(parser.parseExpression("#root").getValue(context, Map.class)).size());
+  }
+
+  @Test
+  public void testCollectionSelection_List() {
+    final String json = readFileContent("shop.json");
+    final Configuration configuration = Configuration.defaultConfiguration()
+        .jsonProvider(new JacksonJsonProvider())
+        .mappingProvider(new JacksonMappingProvider());
+    List<Map<String, Object>> books = JsonPath
+        .using(configuration)
+        .parse(json)
+        .read("$.store.book", new TypeRef<List<Map<String, Object>>>() {
+        });
+    EvaluationContext context = new StandardEvaluationContext();
+    context.setVariable("list", books);
+    ExpressionParser parser = new SpelExpressionParser();
+    Assertions.assertEquals(2, Objects.requireNonNull(
+        parser.parseExpression("#list.?[#this['price']>10]").getValue(context, List.class)).size());
   }
 
 }
